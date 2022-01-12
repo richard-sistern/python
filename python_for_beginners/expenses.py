897ecddca17eb59536fe96cb26cbde43abc9d085
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2022-01-12 16:42:25 +0000

    Add expenses demo

---
+++ python_for_beginners/expenses.py
@@ -0,0 +1,11 @@
+expenses = [10.50, 8, 5, 15, 20, 5, 3]
+
+# sum = 0
+
+# for expense in expenses:
+#     print(expense)
+#     sum = sum + expense
+
+total = sum(expenses)
+    
+print("You spent $", total, " on lunch this week.", sep='')
