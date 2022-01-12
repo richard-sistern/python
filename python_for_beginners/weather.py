7cbf30f3d491adb3e477bed79ae6a80eb9c100b9
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2022-01-12 15:32:53 +0000

    Add weather

---
+++ python_for_beginners/weather.py
@@ -0,0 +1,21 @@
+temp = 50
+
+if temp > 80:
+    print("Hot...")
+elif temp < 60:
+    print("Freezing...")
+else:
+    print("Cold...")
+
+temp = 75
+if temp > 80 or temp < 60:
+    print ("Stay inside!")
+else:
+        print("Enjoy the outdoors!")
+
+
+forecast = "rain"
+
+
+
+print("Done!")
