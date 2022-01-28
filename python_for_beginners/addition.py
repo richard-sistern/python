2efc50c5fbbc963f6126c8c44c3b615e92a669de
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2022-01-28 16:14:54 +0000

    Add addition demo

---
+++ python_for_beginners/addition.py
@@ -0,0 +1,10 @@
+# Addition function
+def addition(a, b):
+    return a + b
+
+# Main
+num1 = float(input('Enter your 1st number: '))
+num2 = float(input('Enter your 2nd number: '))
+
+result = addition(num1, num2)
+print('The result is ', result)
