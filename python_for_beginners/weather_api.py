81b4a13aa07083564abdb09a981778a4b73301bc
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2022-01-31 19:02:46 +0000

    Update weather API

--- python_for_beginners/weather_api.py
+++ python_for_beginners/weather_api.py
@@ -1,14 +1,28 @@
 import requests
 
-api_key = "bla...bla"
-city = "Orlando"
-url = "http://api.openweathermap.oreg/data/2.5/weather?q="+city+"&appid="+api_key+"&units=metric"
+def get_weather_desc_and_temp():
+    api_key = "bla...bla"
+    city = "Orlando"
+    url = "http://api.openweathermap.oreg/data/2.5/weather?q="+city+"&appid="+api_key+"&units=metric"
 
-request = requests.get(url)
-json = request.json()
+    request = requests.get(url)
+    json = request.json()
 
-description = json.get("weather")[0].get("description")
-print("Today's forecase is", description)
+    description = json.get("weather")[0].get("description")
+    temp_min = json.get("main").get("temp_min")
+    temp_max = json.get("main").get("temp_max")
 
-temp_min = json.get("main").get("temp_min")
-print("The minimum temperature is", temp_min)
+    return {
+        'description': description,
+        'temp_min': temp_min,
+        'temp_max': temp_max
+    }
+
+def main():
+    weather_dict = get_weather_desc_and_temp()
+
+    print("Today's forecase is", weather_dict.get('description'))
+    print("The maximum temperature is", weather_dict.get('temp_max'))
+    print("The minimum temperature is", weather_dict.get('temp_min'))
+
+main()
